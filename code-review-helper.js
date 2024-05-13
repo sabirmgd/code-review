acbc9d8bb7e5efa71b8d11bdac59f3793f1d2cb0
sabirmgd <sabirmgd@gmail.com>
2024-05-13 13:14:40 +0300

    Create code-review-helper.js

---
+++ code-review-helper.js
@@ -0,0 +1,15 @@
+function extractStartingLineNumbers(diff) {
+  const regex = /@@\s*-(\d+),\d+\s*\+(\d+),\d+\s*@@/;
+  const match = diff.match(regex);
+
+  if (match) {
+    const newFileStartLine = parseInt(match[2], 10);
+    return {
+      x,
+      newFileStartLine,
+      newFileStartLine,
+    };
+  } else {
+    throw new Error("Invalid diff format", diff);
+  }
+}
